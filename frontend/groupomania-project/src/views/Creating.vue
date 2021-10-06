57cf813b2e2b0833bff242377cd7ef0bc2b35f2a
y4wee <quentin.jambert@gmail.com>
2021-10-06 20:07:20 +0200

    page creation

--- frontend/groupomania-project/src/views/Creating.vue
+++ frontend/groupomania-project/src/views/Creating.vue
@@ -1,16 +1,26 @@
 <template>
 
-    <div class="home">
+    <div class="creating">
         <Nav />
 
-        <div class="homeList">
-            <div class="homeListHead">
-                <h2>Quoi de neuf {{ user.prenom }} ?</h2>
-            </div>
+        <div class="creatingMain">
 
-            <div class="homeListCarte">
+            <h1>Créez une publication </h1>
+            
+            <form class="creatingMainForm" enctype="multipart/form-data">
+                <div class="creatingMainFormTitre">
+                    <input v-model="titre" type="text" name="titre" placeholder="Titre..." required>
+                </div>
 
-            </div>
+                <div class="creatingMainFormMedia">
+                    <input type="file" name="file" ref="file" accept=".png, .jpg, .jpeg, .gif" @change="fileSetting" required>
+                </div>
+
+                <img v-if="image.length > 0" :src="image" alt="aperçu media publication">
+
+                <button @click.prevent="articleCreating()">Créer</button>
+            </form>
+
         </div>
     </div>
 
@@ -21,7 +31,7 @@ import Nav from '../components/Nav.vue';
 import { mapState } from 'vuex';
 
 export default {
-    name: 'Home',
+    name: 'Creating',
     components: {
         Nav
     },
@@ -30,19 +40,57 @@ export default {
             this.$router.push('/');
             return;
         }
-        this.$store.dispatch('articleGetAll');
+    },
+    data() {
+        return {
+            titre: '',
+            file: {},
+            image: ''
+        }
     },
     computed: {
         ...mapState({
-            user: 'user',
-            articles: 'articles'
-        }),
+            user: 'user'
+        })
+    },
+    methods: {
+        fileSetting: function(event) {
+            this.file = event.target.files[0] || event.dataTransfer.files;
+            if (this.file == null) {
+                this.image = '';
+                return;
+            }
+            this.createImage(this.file)
+        },
+        createImage: function(file) {
+            let reader = new FileReader();
+
+            reader.onload = (event) => {
+                this.image = event.target.result;
+            };
+            reader.readAsDataURL(file) 
+        },
+        articleCreating: function() {
+            const formData = new FormData
+            formData.append("file", this.file)
+            formData.append("titre", this.titre)
+            formData.append("UserId", this.user.userId)
+            console.log(this.file);
+            this.$store.dispatch('articleCreating', formData)
+            .then((res) => {
+                console.log(res);
+                this.$router.push('/home');
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        },
     }
 }
 </script>
 
 <style scoped lang="scss">
-.home {
+.creating {
     display: flex;
     align-items: center;
     flex-direction: column;
@@ -50,14 +98,83 @@ export default {
     min-height: 100vh;
     background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(249,249,249,1) 46%, #aeaeb1 100%);
 
-    &List {
+    &Main {
         position: relative;
         z-index: 2;
         width: 800px;
-        min-height: calc(100vh - 100px);
+        height: calc(100vh - 70px);
         background-color: white;
-        margin-top: 100px;
-        box-shadow: 0 10px 10px 6px #d1515a;
+        margin-top: 70px;
+        box-shadow: 0 8px 4px 1px #d1515a;
+        overflow-y: scroll;
+        overflow-x: hidden;
+        &::-webkit-scrollbar {
+            width: 6px;
+        }
+        &::-webkit-scrollbar-thumb {
+            background-color: #aeaeb1;
+            border-radius: 30px;
+        }
+        & h1 {
+            text-align: center;
+            color: #aeaeb1;
+        }
+        // aspect formulaire post publication
+        &Form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background-color: #F4F4F4;
+            width: 550px;
+            margin: 50px auto;
+            padding: 20px;
+            border-radius: 8px;
+            & input {
+                outline: none;
+                border: none;
+                font-size: 1.1em;
+                width: 80%;
+            }
+            &Titre {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 80%;
+                height: 50px;
+                border-radius: 8px;
+                background-color: white;
+                margin: 10px 0;
+            }
+            &Media {
+                width: 80%;
+                margin: 10px 0;
+                & input {
+                    color: #122441;
+                }
+            }
+            & img {
+                max-height: 400px;
+                max-width: 350px;
+                margin: 15px auto;
+                border-radius: 8px;
+            }
+            & button {
+                margin-top: 10px;
+                width: 80px;
+                height: 35px;
+                border: none;
+                border-radius: 8px;
+                background-color: #d1515a;
+                color: white;
+                cursor: pointer;
+                font-size: 1em;
+                transition: all 0.15s ease-out;
+                &:hover {
+                    transform: scale(1.1);
+                    box-shadow: 0 0 5px 0 #aeaeb1;
+                }
+            }
+        }
     }
 }
 </style>
